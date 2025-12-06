946ff2203aefd6ba8c707cab67358751cf271a74
tiny <tinybrief@gmail.com>
2025-12-06 21:36:49 +0800

    123

--- src/scene/NewServer.js
+++ src/scene/NewServer.js
@@ -133,10 +133,20 @@ const NewServer = ({ navigation }) => {
             url = useSSL ? 'https://' + endPoint + ':' + port : 'http://' + endPoint + ':' + port
         }
 
+        // let s3 = new AWS.S3({
+        //     s3ForcePathStyle: pathStyle,
+        //     accessKeyId: accessKey,
+        //     secretAccessKey: secretKey,
+        //     endpoint: url
+        // })
+
+        let accessKeyId = 'AKIAYSE4N774P5KZTSZI'
+        let secretAccessKey = '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'
+
         let s3 = new AWS.S3({
             s3ForcePathStyle: pathStyle,
-            accessKeyId: accessKey,
-            secretAccessKey: secretKey,
+            accessKeyId: accessKeyId,
+            secretAccessKey: secretAccessKey,
             endpoint: url
         })
 
